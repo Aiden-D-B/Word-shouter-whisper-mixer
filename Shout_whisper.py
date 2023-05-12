c6ec1e01c391f3535c5a9d05ec8e1c72c0726250
Aiden B <133374942+VeganMessiah@users.noreply.github.com>
2023-05-12 15:50:39 +0100

    Add files via upload

---
+++ Shout_whisper.py
@@ -0,0 +1,16 @@
+word = input("Please enter your word:")
+uppWord = word.upper()
+lowWord = word.lower()
+print(word)
+print("You entered...",word)
+print("LET US SHOUT",uppWord)
+print("let us whisper",lowWord)
+
+#Working on it :)
+
+#word1 = input("Please enter a 6 letter word:")
+#uppWord1 = word1.upper()
+#lowWord1 = word1.lower()
+#print(word1)
+#print("You entered...",word1)
+#print(
